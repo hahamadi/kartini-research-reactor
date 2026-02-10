71e4fb3f9a95c42064e4a4761e31c428c3557e0c
hahamadi <halim.batan18@gmail.com>
2026-02-10 13:49:14 +0700

    third commit

--- clasic_method_V2.py
+++ clasic_method_V2.py
@@ -9,7 +9,6 @@ excel_list = os.path.join(os.getcwd(),"data_operasi_reaktor")
 
 TIME_COL = "Time"
 ROD_COL = "Regulator Rod [%]"
-PWR_COL_CANDIDATES = ["Power NP1000 [kW]", "Power NLW2 [kW]"]
 
 # List semua file Excel
 excel_files = []
@@ -20,6 +19,7 @@ for file in os.listdir(excel_list):
 print(f"Jumlah file Excel di folder '{current_folder}': {len(excel_files)}")
 
 data_excel_list = []
+excel_files_short = []
 if excel_files:
     # Filter hanya file dengan format tanggal DD_MM_YYYY
     dated_files = []
@@ -31,8 +31,11 @@ if excel_files:
         dated_files.sort(key=lambda x: re.search(r'(\d{2})_(\d{2})_(\d{4})', x).groups()[::-1])
         for file in dated_files:
             excel_name = os.path.join(excel_list,file)
+            excel_files_short.append(file)
             data_excel_list.append(excel_name)
-            
+
+print(f"nama file pertama '{excel_files_short[0]}', nama file terakhir '{excel_files_short[-1]}'.")
+        
 def load_one_file(path):
     df = pd.read_excel(path, sheet_name="Download Transaksi", header=1)
     # parse time
@@ -49,7 +52,7 @@ def load_one_file(path):
 
     df["Time_sec"] = (df["Time"] - df["Time"].iloc[0]).dt.total_seconds()
     df["dt_s"] = dt_s
-    df["rod_speed_using_diff"] = drod / dt_s
+    df["v_reg_diff"] = drod / dt_s
     return df
 
 def forward_central_diff(path):
@@ -72,6 +75,26 @@ def forward_central_diff(path):
     v_central[1:-1] = (x[2:] - x[:-2]) / (t[2:] - t[:-2])
     df["v_reg_central"] = v_central
     return df
-    
 
-print(f"nama file pertama {data_excel_list[0]}, nama file terakhir {data_excel_list[-1]}")
+df1 = load_one_file(data_excel_list[0])
+df2 = forward_central_diff(data_excel_list[0])
+
+print(len(df1["v_reg_diff"]), len(df2["v_reg_central"]))
+
+plt.figure()
+plt.plot(df1["Time_sec"], df1["Regulator Rod [%]"])
+plt.xlabel("Time (s)")
+plt.ylabel("Regulator position (%)")
+plt.title("Regulating Rod Position vs Time")
+plt.grid()
+plt.show()
+
+plt.figure()
+plt.plot(df1["Time_sec"], df1["v_reg_diff"])
+plt.plot(df1["Time_sec"], df2["v_reg_forward"])
+plt.plot(df1["Time_sec"], df2["v_reg_central"])
+plt.xlabel("Time (s)")
+plt.ylabel("Rod speed (%/s)")
+plt.title("Regulating Rod Speed vs Time")
+plt.grid()
+plt.show()
